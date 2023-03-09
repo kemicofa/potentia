8155da1cc76472eee5ab11ca1ada2801ace1bf63
kevinf <kevin.faulhaber@gmail.com>
2023-03-09 23:06:47 +0100

    v0.0.1

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,8 +2,15 @@ import * as React from "react";
 
 export default function Index() {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="w-screen h-screen bg-lichking flex justify-center items-center bg-cover bg-no-repeat bg-gradient-to-b">
+      <div className="flex flex-col justify-center items-center">
+        <h1 className='font-title font-black text-9xl animate-pulse-ts'>Potentia</h1>
+        <p className="text-3xl">Earthshaker - Alliance</p>
+        <button className="px-4 py-2 bg-orange-900 text-white flex items-center">
+          <img src="/discord-50.png" alt="discord icon" className='w-6 mr-4 invert'/>
+          Apply today
+        </button>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        lichking: "url('/lichking.jpg')",
+      },
+      animation: {
+        'pulse-ts': 'pulse-ts 5s infinite'
+      },
+      keyframes: {
+        'pulse-ts': {
+          '50%': {
+            'text-shadow': '0 0 30px blue'
+          }
+        }
+      }
+    },
+    fontFamily: {
+        title: 'Cinzel',
+        sans: ['Roboto', 'sans-serif']
+    }
   },
   plugins: [],
 }

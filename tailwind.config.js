38a61bcd93b9bd253a6240ea4f1c08b5bb8ee6a3
kevinf <kevin.faulhaber@gmail.com>
2023-03-12 11:22:32 +0100

    fix: discord color and some css for text

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      colors: {
+        'discord-purple': '#6A5ACD'
+      },
       backgroundImage: {
         lichking: `
           linear-gradient(
